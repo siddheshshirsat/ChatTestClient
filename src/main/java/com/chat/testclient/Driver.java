01df1d622b87dfc779bc36f26493205d351071e7
Shirsat <shirsats@amazon.com>
2019-12-08 23:47:33 -0800

    Scalable testing

--- src/main/java/com/chat/testclient/Driver.java
+++ src/main/java/com/chat/testclient/Driver.java
@@ -1,11 +1,15 @@
 package com.chat.testclient;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.apache.log4j.PropertyConfigurator;
 
 public class Driver {
+	private static final int NUMBER_OF_USERS = 2; 
 	
 	private static ExecutorService executorService = Executors.newFixedThreadPool(10);
 
@@ -14,8 +18,21 @@ public class Driver {
 
     	ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger("org.apache.http");
         root.setLevel(ch.qos.logback.classic.Level.INFO);
-
-    	executorService.submit(new UserTask("testUserId1"));
-    	executorService.submit(new UserTask("testUserId2"));
+        
+        List<Future<Integer>> resultFutures = new ArrayList<>();
+        for(int i=0; i<NUMBER_OF_USERS; i++) {
+        	resultFutures.add(executorService.submit(new UserTask("testUserId" + i, NUMBER_OF_USERS)));
+        }
+        
+        int resultSum = resultFutures.stream().mapToInt(f -> {
+			try {
+				return f.get();
+			} catch (Exception e) {
+				e.printStackTrace();
+				return 0;
+			}
+		}).sum();
+        
+        System.out.println("resultSum = " + resultSum);
     }
 }
--- src/main/java/com/chat/testclient/UserTask.java
+++ src/main/java/com/chat/testclient/UserTask.java
@@ -23,12 +23,16 @@ import com.chat.connectionmanager.model.Message;
 import com.chat.connectionmanager.model.RequestConnectionRequest;
 import com.chat.connectionmanager.model.RequestConnectionResponse;
 import com.chat.connectionmanager.model.SendMessageRequest;
+import com.chat.connectionmanager.model.SendMessageResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.Getter;
 
 @ClientEndpoint
-public class UserTask implements Callable<Boolean> {
+public class UserTask implements Callable<Integer> {
+	private static final int MESSAGES_TO_SEND = 10;
+	private static final long MAX_WAIT_TIME_MILLIS = 5000;
+
 	private static final String CONNECTION_MANAGER_ENDPOINT = "http://localhost:9000";
 	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 	private static final String CONNECTION_API = "/user";
@@ -38,14 +42,18 @@ public class UserTask implements Callable<Boolean> {
 	@Getter
 	private String userId;
 
+	@Getter
+	private int totalNumberOfUsers;
+
 	HttpClient httpClient;
 
-	public UserTask(String userId) {
+	public UserTask(String userId, int totalNumberOfUsers) {
 		this.userId = userId;
+		this.totalNumberOfUsers = totalNumberOfUsers;
 		httpClient = HttpClients.createDefault();
 	}
 
-	public Boolean call() throws Exception {
+	public Integer call() throws Exception {
 		RequestConnectionRequest connectionRequest = new RequestConnectionRequest();
 		connectionRequest.setUserId(userId);
 		HttpPost post = new HttpPost(CONNECTION_MANAGER_ENDPOINT + REQUEST_CONNECTION_ENDPOINT);
@@ -57,14 +65,15 @@ public class UserTask implements Callable<Boolean> {
 		RequestConnectionResponse connectionResponse = OBJECT_MAPPER
 				.readValue(EntityUtils.toString(response.getEntity()), RequestConnectionResponse.class);
 		setupConnection(connectionResponse);
-		
+
 		// random async. message sending
-		for(int i=0; i<2;i++) {
-			int waitTime = (int)(Math.random() * 5) + 1;
-			Thread.sleep(waitTime* 1000);
-			sendMessage();
+		int count = 0;
+		for (int i = 0; i < MESSAGES_TO_SEND; i++) {
+			int waitTime = (int) (Math.random() * MAX_WAIT_TIME_MILLIS) + 1;
+			Thread.sleep(waitTime);
+			count += sendMessage();
 		}
-		return true;
+		return count;
 	}
 
 	private void setupConnection(RequestConnectionResponse connectionResponse) throws IOException, URISyntaxException {
@@ -73,8 +82,9 @@ public class UserTask implements Callable<Boolean> {
 		try {
 			container = ContainerProvider.getWebSocketContainer();
 			System.out.println("Reached..." + connectionResponse);
-			session = container.connectToServer(UserWebsocketClientEndpoint.class, URI.create(connectionResponse.getScheme() + "://" + connectionResponse.getEndpoint() + CONNECTION_API));
-	        session.getAsyncRemote().sendText("Connect:" + userId);
+			session = container.connectToServer(UserWebsocketClientEndpoint.class, URI.create(
+					connectionResponse.getScheme() + "://" + connectionResponse.getEndpoint() + CONNECTION_API));
+			session.getAsyncRemote().sendText("Connect:" + userId);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -85,14 +95,26 @@ public class UserTask implements Callable<Boolean> {
 		System.out.println("Received msg: " + message);
 	}
 
-	private void sendMessage() throws IOException, URISyntaxException {
-		String recipientId = "testUserId1".equals(userId) ? "testUserId2" : "testUserId1";
+	private int sendMessage() throws IOException, URISyntaxException {
+		int recipientIndex = (int) (Math.random() * totalNumberOfUsers);
+
+		String recipientId = "testUserId" + recipientIndex;
 		Message message = new Message(userId, recipientId, "Random message", System.currentTimeMillis());
 		SendMessageRequest sendMessageRequest = new SendMessageRequest(message);
 		HttpPost post = new HttpPost(CONNECTION_MANAGER_ENDPOINT + SEND_MESSAGE_API);
 
-		post.setEntity(new StringEntity(OBJECT_MAPPER.writeValueAsString(sendMessageRequest), ContentType.APPLICATION_JSON));
+		post.setEntity(
+				new StringEntity(OBJECT_MAPPER.writeValueAsString(sendMessageRequest), ContentType.APPLICATION_JSON));
 
-		httpClient.execute(post);
+		HttpResponse response = httpClient.execute(post);
+		SendMessageResponse sendMessageResponse = OBJECT_MAPPER.readValue(EntityUtils.toString(response.getEntity()),
+				SendMessageResponse.class);
+
+		if (sendMessageResponse.isDelivered()) {
+			return 1;
+		} else {
+			return 0;
+		}
+//		System.out.println("Message sent successfully for user = " + userId);
 	}
 }
